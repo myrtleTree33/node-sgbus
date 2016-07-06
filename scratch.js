56c3bfac8c4cc5e1585fc26e518c7d7f610548a9
myrtletree33 <me@joeltong.org>
2016-07-06 17:24:09 +0800

    added common module to pull paginated requests repeatedly

--- scratch.js
+++ scratch.js
@@ -1,9 +1,10 @@
 var Future = require('fibers/future'),
   request = Future.wrap(require('request'));
 
-var config = require('./config.json');
+var Common = function() {
+};
 
-var retrieve = function(params) {
+var performRequest = function(params) {
   var result = request({
     url: params.url,
     method: params.method,
@@ -11,16 +12,48 @@ var retrieve = function(params) {
       AccountKey: params.key,
       UniqueUserID: params.uuid,
       accept: 'application/json'
-    }
+    },
+    qs: params.qs
   }).wait();
-  return result.body;
+  return JSON.parse(result.body);
+}
+
+/**
+ * Perform a single request
+ * @return {[type]} [description]
+ */
+Common.prototype.retrieve = performRequest.future();
+
+
+/**
+ * Perform multiple requests
+ * @param  {[type]} params [description]
+ * @return {[type]}        [description]
+ */
+Common.prototype.multipleRetrieve = function(params) {
+  var items = [];
+  var count = 0;
+  while (true) {
+    params.qs = {
+      $skip: count
+    }
+    var response = performRequest(params);
+    items = items.concat(response.value);
+    if (response.value.length < 50) {
+      return items;
+    } else {
+      count += 50;
+    }
+  }
 }.future();
 
-retrieve({
-  url: 'http://datamall2.mytransport.sg/ltaodataservice/BusStops',
-  method: 'GET',
-  key: config['AccountKey'],
-  uuid: config['UniqueUserID']
-}).resolve(function(err, val) {
-  console.log(val);
-});
+// multipleRetrieve({
+//   url: 'http://datamall2.mytransport.sg/ltaodataservice/BusStops',
+//   method: 'GET',
+//   key: config['AccountKey'],
+//   uuid: config['UniqueUserID']
+// }).resolve(function(err, val) {
+//   console.log('done');
+// });
+
+module.exports = new Common();
